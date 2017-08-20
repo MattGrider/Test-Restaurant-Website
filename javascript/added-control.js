6910a8a47e8929f82ee43fb6e879b6714e012741
MattGrider <mgride3@uic.edu>
2017-08-20 12:22:20 -0500

    adding to menu

--- javascript/added-control.js
+++ javascript/added-control.js
@@ -4,13 +4,17 @@
 	"use strict";
 	$(document).ready(function($) {
   	$('#menu-container').click(function() {
-		var menu = document.getElementById('main-menu');
+		var menu = document.getElementById('menu-display');
+		var collapseBackground = document.getElementsByClassName('cutting-background');
     	if(menu.style.visibility === 'hidden')
 		{
 			menu.style.visibility = 'visible';
+			collapseBackground[0].style.backgroundColor = "white";
+			collapseBackground[0].style.backgroundImage = "url('')";
 		}
 		else
 		{
+			collapseBackground[0].style.backgroundImage = 'url(img/cuttingboard2.png)';
 			menu.style.visibility = 'hidden';
 		}
   	});
