da0e1f705e604967b5fdbf0bd182ca507f1e5f4f
MattGrider <mgride3@uic.edu>
2017-08-24 23:01:30 -0500

    removed menu all together just use nav bar

--- javascript/added-control.js
+++ javascript/added-control.js
@@ -27,6 +27,27 @@
 	$('#navbarload').css('opacity', '1');
 	$('#social').css('opacity', '1');
 	$('#menu-container').css('opacity', '1');
+	
+	$('#navmenubutton').click(function() {
+		
+		var collapseBackground = document.getElementsByClassName('cutting-background');
+    	if(collapseBackground[0].style.backgroundimage === 'url(img/cuttingboardmobile.png)' || collapseBackground[0].style.backgroundimage === 'url(img/cuttingboard2.png)')
+		{
+			collapseBackground[0].style.backgroundColor = "white";
+			collapseBackground[0].style.backgroundImage = "url('')";
+		}
+		else
+		{
+			if (screen.width < 500) {
+    			collapseBackground[0].style.backgroundImage = 'url(img/cuttingboardmobile.png)';
+			}
+			else
+			{
+				collapseBackground[0].style.backgroundImage = 'url(img/cuttingboard2.png)';
+			}
+			
+		}
+  	});
 		
   	$('#menu-container').click(function() {
 		
